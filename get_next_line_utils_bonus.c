666f10addd4c61ff387f77219506e3bb122cbe83
Fredrika Lindh <fredrikalindh@gmail.com>
2019-10-28 19:52:03 +0100

    kanske klar

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,29 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: frlindh <frlindh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/10 14:09:36 by frlindh           #+#    #+#             */
-/*   Updated: 2019/10/28 15:30:09 by fredrikalindh    ###   ########.fr       */
+/*   Created: 2019/10/26 11:47:29 by frlindh           #+#    #+#             */
+/*   Updated: 2019/10/26 11:56:05 by frlindh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-
-int		get_next_line(int fd, char **line);
-
-#endif
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
-# endif
+#include "get_next_line.h"
